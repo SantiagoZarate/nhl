c54564a1904a91e4694a132ee74254e5b85a1b23
Santiago Zarate <santiago2001zarate@gmail.com>
2024-05-04 18:47:02 -0300

    chore: repo initialized

---
+++ backend/src/test/java/com/santiago/NHL/NhlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santiago.NHL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
